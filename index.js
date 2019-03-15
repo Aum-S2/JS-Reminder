9b9dcb3040c5a5081912bbf59a0bb497b3757e8e
Aum-S2 <sorasitdb7375@hotmail.com>
2019-03-15 14:32:55 +0700

    variable

---
+++ index.js
@@ -0,0 +1,13 @@
+let name = 'Aum';
+console.log(name);
+
+// cannot be a reserved keyboard
+// should be meaningful
+// cannot start with a number (like '123aum')
+// cannot contain space or hyphen (-)
+// are case-sensitive ('firstName')/ ('FirstName') <--- this 2 are different
+
+let firstName = 'Panda';
+let lastName = 'Nawari';
+
+// you should declear variable on a single line because it easy to read
