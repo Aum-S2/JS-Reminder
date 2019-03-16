6c191bfc0a54d5649f7c09ad91ea9421e16beca5
Aum-S2 <sorasitdb7375@hotmail.com>
2019-03-16 14:48:43 +0700

    let const

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ let firstName = 'Panda';
 let lastName = 'Nawari';
 
 // you should declear variable on a single line because it easy to read
+
+let interestRate = 0.2;
+interestRate = 1;
+console.log(interestRate);
+
+// use const if you dont need to reassign
+// use let if you need to reassign 
